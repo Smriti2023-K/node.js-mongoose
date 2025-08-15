29d660cc1d49ba757e5d208ccec59158a5da8f7d
Smriti Kaushik <smritikaushik001@gmail.com>
2025-08-15 23:44:49 +0530

    update,delete,find

--- index.js
+++ index.js
@@ -3,11 +3,74 @@ const mongoose = require ('mongoose');
 const main = async () =>{ 
     await mongoose.connect("mongodb://localhost:27017/e-comme");
     const PS = new mongoose.Schema({
-        name:String
+        name:String,
+        price:Number,
+        brand:String,
+        category:String
     });
-    const PsM = mongoose.model('products-m' , PS);
-    let data = new PsM({name:"m8",price:'800'});
-        let result = await data.save();
-    console.log(result)
+
+
+    ///// insert and read///////////
+
+    // const PsM = mongoose.model('products-m' , PS);
+    // let data = new PsM({
+    //     name:"iphone 11",
+    //     price:'800',
+    //     brand:'iphone',
+    //     category:'Mobile'
+    // });
+    //     let result = await data.save();
+    // console.log(result)
+
+    // update////
+
+//     const updateInDB = async() =>{
+//         const Product = mongoose.model('products-ms', PS);
+//         let data = await Product.updateOne(
+//             {name : "m8"},
+//             { $set : {price :1700}
+//         }
+//         )
+//     console.log(data);
+//     }
+// updateInDB()
+
+
+
+// const deleteInDB = async () =>{
+//     const Product = mongoose.model('products-ms' , PS);
+//     let data = await Product.deleteOne({name:'m8'});
+//     console.log(data);
+// }
+// deleteInDB();
+
+
+//find /////////////////
+
+// const findInDB = async () =>{
+//     const Product = mongoose.model('products-ms', PS);
+//     let data = await Product.find();
+//     console.log(data);
+//   } 
+//     findInDB();
+
+/// update///////
+
+const updateInDB = async () =>{
+    const Product = mongoose.model('products-ms', PS)
+    let data = await Product.updateOne (
+        { name :'iphone 11'},
+        {
+            $set:{ price:700}
+        }
+    ); 
+    console.log (data);
 }
+
+updateInDB()
+
+}
+
 main()
+
+
